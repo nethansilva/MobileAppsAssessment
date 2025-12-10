648facb9f5f321318b5c5651e580a8409308be64
nethan <nethansilvar34@gmail.com>
2025-12-10 12:59:40 +1100

    Add icons to the navigation menu.

--- src/_layout.js
+++ src/_layout.js
@@ -1,8 +1,10 @@
 import Events from "@/screens/Events";
 import Home from "@/screens/Home";
 import Settings from "@/screens/Settings";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-paper";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,17 +14,17 @@ export default function TabsLayer() {
 			<Tab.Screen
 				name="home"
 				component={Home}
-				options={{ title: "Welcome" }}
+				options={{ title: "Welcome", tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="home" size={size} color={color} /> }}
 			/>
 			<Tab.Screen
 				name="events"
 				component={Events}
-				options={{ title: "Events" }}
+				options={{ title: "Events", tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="calendar" size={size} color={color} />  }}
 			/>
 			<Tab.Screen
 				name="settings"
 				component={Settings}
-				options={{ title: "Settings" }}
+				options={{ title: "Settings", tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="cog" size={size} color={color} />  }}
 			/>
 		</Tab.Navigator>
 	);
